ea9dd46e6ebd32e4871ee1878c7309e160fda8d8
md-gulam-quddus <sohag.1984@gmail.com>
2023-01-05 15:27:06 +0100

    save user and jwt token initialized

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
@@ -10,11 +11,46 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.h6dt8.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const productsCollection = client.db("Ubuy").collection("products");
+    const usersCollection = client.db("Ubuy").collection("users");
+
+    //Save user and genarate jwt token
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1d",
+      });
+
+      res.send({ result, token });
+    });
+
+    console.log("Database Connected...");
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("Server is Running");
